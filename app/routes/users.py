a93b4ad9e6a388df20e41ddafcdc56b83b504651
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-03-05 15:26:16 +0530

    http methods for routes

--- app/routes/users.py
+++ app/routes/users.py
@@ -2,13 +2,14 @@ from flask import request, make_response
 from app.services.user_service import UserService
 from . import users_bp
 
-@users_bp.route('/', methods=['POST'])
+@users_bp.post('')
 def create_user():
+    print("get_hit")
     data = request.get_json()
     response, status = UserService.create_user(data.get('name'), data.get('email'), data.get('password'))
     return make_response(response, status)
 
-@users_bp.route('/', methods=['GET'])
+@users_bp.get('')
 def get_all_users():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
@@ -16,7 +17,7 @@ def get_all_users():
     response, status = UserService.get_all_users(page, per_page)
     return make_response(response, status)
 
-@users_bp.route('/<int:user_id>', methods=['GET'])
+@users_bp.get('/<int:user_id>')
 def get_user(user_id: int):
     response, status = UserService.get_user(user_id)
     return make_response(response, status)
