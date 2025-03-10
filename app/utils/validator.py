e2de8c7fb26fdddb6ab71b6f7f3decedf42d6682
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-03-10 10:30:12 +0530

    category validation

--- app/utils/validator.py
+++ app/utils/validator.py
@@ -8,3 +8,8 @@ class UserSchema(Schema):
 class LoginSchema(Schema):
     email = fields.Email(required=True)
     password = fields.String(required=True, validate=validate.Length(min=6))
+
+class CategorySchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(min=2, max=100))
+    parent_id = fields.Int(allow_none=True)
