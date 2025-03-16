e3bcb21f21f96a3ffbdc6a55aad058e2d6d690e0
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-03-16 17:40:45 +0530

    admin route registeration

--- app/__init__.py
+++ app/__init__.py
@@ -12,11 +12,13 @@ def create_app() -> Flask:
     app.config.from_object('app.config.Config')
     db.init_app(app)
 
-    from app.routes import users_bp, categories_bp, cart_bp, products_bp
+    from app.routes import users_bp, categories_bp, cart_bp, products_bp, admin_bp
+
     app.register_blueprint(cart_bp, url_prefix='/api/v1/cart')
     app.register_blueprint(products_bp, url_prefix='/api/v1/products')
     app.register_blueprint(users_bp, url_prefix='/api/v1/users')
     app.register_blueprint(categories_bp, url_prefix='/api/v1/categories')
+    app.register_blueprint(admin_bp, url_prefix='/api/v1/admin')
 
     with app.app_context():
         db.drop_all()
